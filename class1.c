7e1ea544d5aaf8e988ca0749b2e0b4f5d987ad1a
Abhishek <abhishekpal941163@gmail.com>
2023-01-17 09:41:04 -0800

    first commit

---
+++ class1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    // data types
+    // bcsilfd
+
+    // Data Types
+
+   /* b- byte
+    c- char
+    s-short
+    i-int
+    l- long
+    f-float    
+    d-double 
+    */
+
+    printf("Hello World\neveryone");
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+// w.t.p to print good moorning everone  in next line
+// 
